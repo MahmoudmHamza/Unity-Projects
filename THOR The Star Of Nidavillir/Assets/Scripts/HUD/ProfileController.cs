98b5aeda7ad3911110d73688b19edbcb49d58193
Mahmoud Hamza <mahmoudmhamza96@gmail.com>
2021-01-04 15:55:50 +0200

    Update Project

---
+++ THOR The Star Of Nidavillir/Assets/Scripts/HUD/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ProfileController : MonoBehaviour
+{
+    private const string LinkedinProfileUrl = "https://www.linkedin.com/in/mahmoud-hamza96/";
+    private const string GithubProfileUrl = "https://github.com/MahmoudmHamza/Unity3D-Game-Development-Projects";
+
+    public void OnLinkedinButtonClicked()
+    {
+        Application.OpenURL(LinkedinProfileUrl);
+    }
+
+    public void OnGithubButtonClicked()
+    {
+        Application.OpenURL(GithubProfileUrl);
+    }
+}
---
+++ THOR The Star Of Nidavillir/README.md
@@ -0,0 +1,17 @@
+>**Brief**
+
+**THOR: The star of Nidavellir** is a 2D game inspired by Marvel's Avengers: End Game movie.
+
+![](https://github.com/MahmoudmHamza/Unity-Projects/blob/master/THOR%20The%20Star%20Of%20Nidavillir/Screenshots/THOR1.PNG)
+
+>**Game Features**
+
+To be added.
+
+>**Supported Platforms**
+
+* PC
+
+>**Download**
+
+Download from [here](https://drive.google.com/file/d/1DhqFbQnQbTNlu5sEjjvSiP5zp0DdsM5P/view?usp=sharing).
